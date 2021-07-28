9e33b80dfb0213a3492ff8350f212b97881cecba
claudegose <84382636+claudegose@users.noreply.github.com>
2021-07-28 16:48:18 +0200

    Project Completed

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>The Dream</title>
 </head
-<body align="center">
+<body>
 <h1>Currency Converter</h1>
 <br>
 <br>
 <br>
+<div class="box">
 <form action="index.php" method="get">
 Enter Currency : <input type="text" name="input" />
  Select Currency :
@@ -24,7 +26,7 @@ Enter Currency : <input type="text" name="input" />
     <input type="submit" name="sbmt" value="Convert" />
 
 </form>
-
+</div>
 </body>
 </html>
 
@@ -36,11 +38,21 @@ if(isset($_GET['sbmt']))
     $cc_input = $_GET['input'];
     $cc_dropdown = $_GET["Dropdown"];
 
-     if($cc_dropdown == "USD")
-     {
-            $output = $cc_input * 1.1810;
-            echo $output . " Dollar";
+     if($cc_dropdown == "USD") {
+            $output = $cc_input / 1.1810;
+            echo $output . " Dollars";
         }
+     else if($cc_dropdown == "GBP") {
+            $output = $cc_input / 0.85;
+            echo $output . " Pounds";
+
+     }  else if($cc_dropdown == "ZAR") {
+         $output = $cc_input / 17.4;
+         echo $output . " Rands";
+     }
+     else {
+         echo "FAIL";
+     }
 }
 
 
